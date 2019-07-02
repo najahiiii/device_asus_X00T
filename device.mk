06c9bd3631d4129e7ae6508137947e3f00b69943
Rizky Benggolo <travarilo@gmail.com>
2019-07-02 13:05:40 +0000

    X00T: Build Dirac
    Change-Id: Ie6108623856e6317d1fd36408768a79106b4e948

--- device.mk
+++ device.mk
@@ -147,6 +147,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.configstore@1.0-service
 
+# Dirac
+PRODUCT_PACKAGES += \
+    Dirac
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
