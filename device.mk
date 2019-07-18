549432717cc21462616e5f6573cee1425b42ec3c
najahiiii <najahiii@outlook.co.id>
2019-07-18 21:43:37 +0700

    X00T: Kang power-libperfmgr 1.3 from wayne
    Signed-off-by: najahiiii <najahiii@outlook.co.id>

--- device.mk
+++ device.mk
@@ -331,10 +331,10 @@ PRODUCT_ENFORCE_RRO_TARGETS := \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service
+    android.hardware.power@1.3-service-libperfmgr
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # Prebuilt Apps
 PRODUCT_PACKAGES += \
